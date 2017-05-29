7b7bc44e8bc9f03e84e54e643aec09acc9f65d0b
kr3110 <n.nkr3110@gmail.com>
2017-05-29 15:50:18 -0400

    hello word

---
+++ hello/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>Hello World!!!</h1>
+        </div>
+        <div className="body-text">
+       <h2>This is a hello world in react</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
